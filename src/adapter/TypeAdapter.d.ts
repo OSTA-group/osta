40e018431d9e715e2c4037cd1e2326d33f485c3d
BauwenDR <BauwenDR@gmail.com>
2024-05-24 11:26:28 +0200

    TypeAdapter documentation formatting fix

--- src/adapter/TypeAdapter.d.ts
+++ src/adapter/TypeAdapter.d.ts
@@ -12,17 +12,17 @@ import {
 
 /**
  * Registers an adapter with the specified methods.
- * @param type {string} The type of the adapter.
- * @param extensionDownload {(params: MarketplaceExtension) => Promise<Extension>} A function that downloads an extension from a marketplace.
- * @param extensionUninstall {(params: Extension) => Promise<void>} A function that uninstalls an extension.
- * @param landmarkDownload  {(params: AdapterMethodParameters) => Promise<SourceLandmark[]>} A function that downloads landmarks associated with a source.
+ * @param {string} type The type of the adapter.
+ * @param {(params: MarketplaceExtension) => Promise<Extension>} extensionDownload A function that downloads an extension from a marketplace.
+ * @param {(params: Extension) => Promise<void>} extensionUninstall A function that uninstalls an extension.
+ * @param {(params: AdapterMethodParameters) => Promise<SourceLandmark[]>} landmarkDownload A function that downloads landmarks associated with a source.
  */
 declare function registerAdapter(type: string, extensionDownload: (params: MarketplaceExtension) => Promise<Extension>, extensionUninstall: (params: Extension) => Promise<void>, landmarkDownload: (params: AdapterMethodParameters) => Promise<SourceLandmark[]>): void
 
 /**
  * Installs an extension of the specified type.
- * @param type {string} The type of the extension to install.
- * @param params {MarketplaceExtension} Parameters required for installing the adapter.
+ * @param {string} type The type of the extension to install.
+ * @param {MarketplaceExtension} params Parameters required for installing the adapter.
  * @returns {Promise<Extension>} A Promise resolving to the extension object to be installed.
  * @throws Error if the adapter for the specified type is not found.
  */
@@ -30,8 +30,8 @@ declare async function installExtension(type: string, params: MarketplaceExtensi
 
 /**
  * Uninstalls an extension of the specified type.
- * @param type {string} The type of the extension to uninstall.
- * @param params {Extension} The extension to be uninstalled.
+ * @param {string} type The type of the extension to uninstall.
+ * @param {Extension} params The extension to be uninstalled.
  * @returns {Promise<void>} A Promise that resolves when the adapter specific information for the extension is deleted.
  * @throws Error if the adapter for the specified type is not found.
  */
@@ -39,8 +39,8 @@ declare async function uninstallExtension(type: string, params: Extension): Prom
 
 /**
  * Executes the extension type adapter for the specifed adapter type and extension.
- * @param type {string} The type of the adapter to execute.
- * @param params {Extension} The extension to be executed.
+ * @param {string} type The type of the adapter to execute.
+ * @param {Extension} params The extension to be executed.
  * @returns {Promise<SourceLandmark[]>} A Promise resolving to an array of landmarks associated with the source.
  * @throws Error if the adapter for the specified type is not found.
  */
