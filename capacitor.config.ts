3b9ebabaada0c62b9bf6608788db1cf71b823d18
BauwenDR <BauwenDR@gmail.com>
2024-05-29 15:17:59 +0200

    Reenable auto hide

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -10,7 +10,7 @@ const config: CapacitorConfig = {
   plugins: {
     SplashScreen: {
       launchShowDuration: 1000,
-      launchAutoHide: false,
+      launchAutoHide: true,
       launchFadeOutDuration: 1000,
       backgroundColor: "#ffffffff",
       androidSplashResourceName: "splash",
