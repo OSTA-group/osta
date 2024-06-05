9d71469045ea4f621255b17fc10f682fa1407cb1
deregtervictor <101172722+deregtervictor@users.noreply.github.com>
2024-06-05 09:06:15 +0200

    Remove unversioned files

--- src/services/MarketplaceService.ts
+++
@@ -1,9 +0,0 @@
-import { MarketplaceExtension } from '../types'
-import axios from 'axios'
-
-async function getExtensions(name: string): Promise<MarketplaceExtension[]> {
-  const response = await axios.get(`/api/extensions?nameContains=${name}`)
-  return response.data
-}
-
-export default { getExtensions }
