168f533b5d52e7a31afbf93c460aa97311c32c36
BauwenDR <BauwenDR@gmail.com>
2024-07-23 19:41:40 +0200

    Upadted android version and changed cluster zoom levels

--- src/pages/MapScreen.tsx
+++ src/pages/MapScreen.tsx
@@ -83,7 +83,7 @@ export function MapScreen() {
           {mapDataLoaded && (
             <>
               {!trip.started && (
-                <MarkerClusterGroup chunkedLoading disableClusteringAtZoom={17}>
+                <MarkerClusterGroup chunkedLoading disableClusteringAtZoom={16}>
                   {landmarks.map((landmark: Landmark) => (
                     <LandmarkMarker
                       key={landmark.id}
@@ -114,7 +114,7 @@ export function MapScreen() {
                   )}
 
                   {/* CLUSTER FOR LANDMARK NOT IN A TRIP */}
-                  <MarkerClusterGroup chunkedLoading disableClusteringAtZoom={17}>
+                  <MarkerClusterGroup chunkedLoading disableClusteringAtZoom={16}>
                     {landmarks.map(
                       (landmark: Landmark) =>
                         !landmark.inTrip && (
